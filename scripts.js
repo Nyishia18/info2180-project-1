7d2f7e9092200929088110ee9c860e65b32539af
Nyishia18 <nyishiarobinson26@gmail.com>
2025-10-23 16:44:29 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-/* Add your JavaScript to this file */
+window.onload = function() {
+  const form = document.querySelector('form');
+  const emailInput = document.querySelector('input[type="email"]');
+  const messageDiv = document.querySelector('.message');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = emailInput.value.trim();
+
+    if (email) {
+      messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+    } else {
+      messageDiv.textContent = "Please enter a valid email address.";
+    }
+  });
+};
